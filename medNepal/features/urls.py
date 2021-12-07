4c289363453a5a9f37ac6d886f9f2d1e4cef4731
Rijwol Shakya <shakyarijwol09@gmail.com>
2021-12-07 12:52:21 +0545

    Project Setup of Features Done css, views, urls made layout- Rijwol

---
+++ medNepal/features/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Article Page Path
+    path('article', views.article, name='article'),
+    # Doctor Page Path
+    path('doctorpage', views.searchdoctor),
+    # Pharmacy Page Path
+    path('pharmacy', views.pharmacy),
+    # SearchDoctor Page Path
+    path('searchdoctor', views.searchdoctor),
+]
--- medNepal/features/views.py
+++ medNepal/features/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# =====================Article Page Part========================================
+def article(request):
+    return render(request, 'features/article.html')
+
+# =====================Doctor Page Part========================================
+def doctorpage(request):
+    return render(request, 'features/doctorpage.html')
+
+# =====================Pharmacy Page Part========================================
+def pharmacy(request):
+    return render(request, 'features/pharmacy.html')
+
+# =====================SearchDoctor Page Part========================================
+def searchdoctor(request):
+    return render(request, 'features/searchdoctor.html')
+
+
+
