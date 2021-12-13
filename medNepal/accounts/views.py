e4700a5cc82588b0d89c601ad8e5af8608540554
tsering-hub <ts19sherpa@gmail.com>
2021-12-13 12:57:06 +0545

    login and register tsering

--- medNepal/accounts/views.py
+++ medNepal/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import LoginForm, CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 
 # Create your views here.
 def homepage(request):
@@ -7,13 +9,45 @@ def homepage(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+
+            if user is not None:
+                if not user.is_staff:
+                    #login(request, user)
+                    return redirect('/register')
+                elif user.is_staff:
+                    #login(request, user)
+                    return redirect('/')
+            else:
+                messages.add_message(request, messages.ERROR, "Invalid user name or password")
+                return render(request, 'accounts/login.html', {'form_login': form})
+
     context = {
         'form_login': LoginForm
     }
     return render(request, 'accounts/login.html',context)
 
 def register_user(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "User registered successfully")
+            return redirect('/')
+        else:
+            messages.add_message(request, messages.ERROR, "Something went wrong")
+            return render(request, 'accounts/register.html', {'form_register': form})
+    
     context = {
         'form_register': CreateUserForm,
     }
     return render(request, 'accounts/register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
